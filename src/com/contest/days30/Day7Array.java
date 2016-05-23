c9e891fe31119a38710bc89703fb0a4ca40ebba8
Saroj Kumar Behera <sarojkmb@gmail.com>
2016-05-23 10:05:38 -0400

    Update Day7Array.java

--- src/com/contest/days30/Day7Array.java
+++ src/com/contest/days30/Day7Array.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 
-public class Solution {
+public class Day7Array {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
